a6540a0a2eb25ece4028b05002de4ae91a63f915
Anton Gushcha <ncrashed@gmail.com>
2016-10-09 14:36:07 +0300

    Reexport important operators from servant

--- src/Servant/API/DB.hs
+++ src/Servant/API/DB.hs
@@ -2,5 +2,6 @@ module Servant.API.DB(
     module Reexport
   ) where
 
+import           Servant.API              as Reexport ((:<|>) (..), (:>))
 import           Servant.API.DB.Argument  as Reexport
 import           Servant.API.DB.Procedure as Reexport
