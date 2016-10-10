563d62f6c3108a64b4be590f8cb40c920cc7d234
Anton Gushcha <ncrashed@gmail.com>
2016-10-10 18:26:28 +0300

    Add positional argument

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,8 @@
+0.2.0.0
+=======
+
+* Split `Arg` to `ArgNamed` and `ArgPos`. 
+
 0.1.0.0
 =======
 
--- src/Servant/API/DB/Argument.hs
+++ src/Servant/API/DB/Argument.hs
@@ -7,15 +7,22 @@ Description : Argument for DB stored functions
 Portability : Portable
 -}
 module Servant.API.DB.Argument(
-    Arg
+    ArgPos
+  , ArgNamed
   ) where
 
 import           Data.Typeable
 import           GHC.Generics
 import           GHC.TypeLits
 
--- | DB procedure argument with name and type.
+-- | Positional argument of DB stored function with attached type info.
 --
--- >>> type SquareProcedure = Arg "a" Int :> Procedure "square" Int
-data Arg (name :: Symbol) a
+-- >>> type SquareProcedure = ArgPos Int :> Procedure "square" Int
+data ArgPos a
+  deriving (Generic, Typeable)
+
+-- | Named argument of DB stored function with attached name and type info.
+--
+-- >>> type SquareProcedure = ArgNamed "a" Int :> Procedure "square" Int
+data ArgNamed (name :: Symbol) a
   deriving (Generic, Typeable)
