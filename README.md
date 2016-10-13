95101a69b42ba664136459a2649f980b859d1904
Anton Gushcha <ncrashed@gmail.com>
2016-10-13 17:20:21 +0300

    Add docs to root module and README

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 servant-db
 ==========
+
+The idea of package is to provide [servant](http://haskell-servant.readthedocs.io/en/stable/)-like 
+DSL for specifying an API for functions stored in RDBMS. 
+
+``` haskell
+data RegisterUser = RegisterUser {
+  userName :: Text
+, userPassword :: Text
+, userPhone :: Phone
+, userEmail :: Email
+}
+
+type API = 
+       ArgNamed "user" RegisterUser
+    :> ArgNamed "isAdmin" Bool
+    :> Procedure "registerUser" (Maybe (Only UserId))
+  :<|> ArgPos UserId
+    :> Procedure "getUser" (Maybe User)
+  :<|> Procedure "listUsers" [User]
+```
+
+The library adds three custom combinators:
+* `ArgNamed name a` - named argument of stored function of type `a`.
+
+* `ArgPos a` - unamed argument of stored function of type `a`.
+
+* `Procedure name a` - named stored function with return type `a`. 
+
+Related libraries:
+
+* [servant-db-postgresql](https://github.com/NCrashed/servant-db-postgresql) - 
+    derives client for PostgreSQL with [postgresql-query](postgresql-query) library.
--- src/Servant/API/DB.hs
+++ src/Servant/API/DB.hs
@@ -1,3 +1,41 @@
+{-|
+Module      : Servant.API.DB
+Description : Type level DSL for describing API to DB
+Portability : Portable
+
+The idea of package is to provide <http://haskell-servant.readthedocs.io/en/stable/ servant>-like
+DSL for specifying an API for functions stored in RDBMS.
+
+@
+data RegisterUser = RegisterUser {
+  userName :: Text
+, userPassword :: Text
+, userPhone :: Phone
+, userEmail :: Email
+}
+
+type API =
+       ArgNamed "user" RegisterUser
+    :> ArgNamed "isAdmin" Bool
+    :> Procedure "registerUser" (Maybe (Only UserId))
+  :<|> ArgPos UserId
+    :> Procedure "getUser" (Maybe User)
+  :<|> Procedure "listUsers" [User]
+@
+
+The library adds three custom combinators:
+* `ArgNamed name a` - named argument of stored function of type `a`.
+
+* `ArgPos a` - unamed argument of stored function of type `a`.
+
+* `Procedure name a` - named stored function with return type `a`.
+
+Related libraries:
+
+* <https://github.com/NCrashed/servant-db-postgresql servant-db-postgresql> -
+    derives client for PostgreSQL with <http://hackage.haskell.org/package/postgresql-query postgresql-query> library.
+
+-}
 module Servant.API.DB(
     module Reexport
   ) where
