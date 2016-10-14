f2d44f327109647bb2619a2e2063366cba0938b0
Anton Gushcha <ncrashed@gmail.com>
2016-10-14 12:53:03 +0300

    Fix documentation

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,12 +1,14 @@
-0.2.1.0
+0.2.0.1
 =======
 
-* Add `Default` type.
+* Fix `Servant.API.DB.Procedure` documentation.
 
 0.2.0.0
 =======
 
-* Split `Arg` to `ArgNamed` and `ArgPos`. 
+* Split `Arg` to `ArgNamed` and `ArgPos`.
+
+* Add `Default` type wrapper.
 
 0.1.0.0
 =======
--- src/Servant/API/DB/Procedure.hs
+++ src/Servant/API/DB/Procedure.hs
@@ -17,9 +17,11 @@ import           GHC.TypeLits
 -- | Endpoint for SQL procedure.
 --
 -- Example, stored function that returns current time in seconds:
+--
 -- >>> type SelectTime = Procedure "time" Integer
 --
 -- Example, stored function that squares given 'Int':
+--
 -- >>> type SquareProcedure = Arg "a" Int :> Procedure "square" Int
 data Procedure (name :: Symbol) a
   deriving (Generic, Typeable)
